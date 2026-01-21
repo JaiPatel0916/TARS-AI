a49cf9bc4a32c3c2bec40efd5672d3618422d688
Kartikmohod123 <kartikm.cse22@sbjit.edu.in>
2026-01-21 14:00:07 +0530

    theme changes

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,17 +1,7 @@
 export function Card({ children, className }) {
   return (
     <div
-      className={`
-        relative
-        rounded-2xl
-        border border-pink-500/30
-        text-white
-        shadow-[0_0_40px_rgba(168,85,247,0.25)]
-        hover:shadow-[0_0_70px_rgba(168,85,247,0.45)]
-        transition-all duration-300
-        overflow-hidden
-        ${className}
-      `}
+     
     >
       {/* Subtle Purple Glow */}
       <div className="absolute inset-0 bg-purple-900/10 blur-[120px] -z-10"></div>
