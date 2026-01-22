4f53cb823ce06bf1907de7c129cf38dbb647a3d9
jai099 <131276939+jai099@users.noreply.github.com>
2026-01-22 13:31:28 +0530

    Enhance CoreServices and StickyScroll components: add framer-motion for animations, improve dark mode detection, and restructure Home component layout Added parallax scrolling

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,7 @@
 import Section from "../components/Section";
-
 import StickyScrollRevealDemo from "../components/StickyScrollRevealDemo";
-import { Card, CardHeader, CardContent } from "../components/Card";
+import { Card } from "../components/Card";
 import Navbar from "../components/Navbar";
-
 import Footer from "../components/Footer";
 import CoreServices from "../components/CoreServices";
 
@@ -13,10 +11,20 @@ export default function Home() {
       <Navbar />
       <Section />
       <Card />
-      <StickyScrollRevealDemo />
-      <CoreServices />
-      <Footer/>
+
+      {/* Behind section */}
+      {/* StickyScroll section (must stay clean) */}
+      <div className="relative z-10 ">
+        <StickyScrollRevealDemo />
+      </div>
+
+      {/* CoreServices overlaps from below */}
+      <div className="relative z-20 -mt-[50vh] pointer-events-auto">
+        <CoreServices />
+      </div>
+
+
+      <Footer />
     </div>
   );
 }
-
